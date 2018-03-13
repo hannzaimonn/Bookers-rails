f148223a261b52a811559591d4dc4a89e5468eb9
kengo0804 <kengo0804i@gmail.com>
2018-03-13 20:13:38 +0900

    Initial Commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,43 @@
+class PostsController < ApplicationController
+  def new
+  	@posts = Post.all
+  	@post = Post.new
+  end
+
+  def create
+  	  post = Post.new(post_params)
+  	  post.save
+  	  redirect_to post_path(post)
+  	end
+
+  def index
+  	@posts = Post.all
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def edit
+  	@post = Post.find(params[:id])
+  end
+
+  def update
+  	post = Post.find(params[:id])
+  	post.update(post_params)
+  	redirect_to post_path(post.id)
+  	flash[:notice] = "Book was successfully update"
+  end
+
+  def destroy
+  	post = Post.find(params[:id])
+  	post.destroy
+  	redirect_to posts_new_path
+  	flash[:notice] = "Book was successfully destroyed"
+  end
+
+  private
+  def post_params
+  	  params.require(:post).permit(:title, :body)
+  end
+end
